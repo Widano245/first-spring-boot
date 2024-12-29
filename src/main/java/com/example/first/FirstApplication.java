277f8bb998fb07be4b039a0fb8a2ac2b359154a6
wijdane <wijdane.elbakhouchi24@gmail.com>
2024-12-29 13:57:19 +0100

    Application de Eudiant

--- src/main/java/com/example/first/FirstApplication.java
+++ src/main/java/com/example/first/FirstApplication.java
@@ -1,7 +1,13 @@
 package com.example.first;
 
+import com.example.first.entities.Etudiant;
+import com.example.first.repositories.EtudiantRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class FirstApplication {
@@ -10,4 +16,24 @@ public class FirstApplication {
 		SpringApplication.run(FirstApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner start(EtudiantRepository etudiantRepository ){
+		return args -> {
+			Etudiant e1 =  Etudiant.builder()
+					.age(23)
+					.nom("ouma")
+					.note(15.0)
+					.build();
+			etudiantRepository.save(e1);
+			Etudiant e2 = Etudiant.builder()
+					.age(20)
+					.nom("Khaoula")
+					.note(14.0)
+					.build();
+			etudiantRepository.save(e2);
+			List<Etudiant> listes = etudiantRepository.findAll();
+			for (Etudiant e : listes)
+				System.out.println(e);
+		};
+	}
 }
